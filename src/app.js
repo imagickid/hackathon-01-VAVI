ccf58f2e95472bb5c53168c724129531023b9244
vitalyvitmens <otm_@tut.by>
2023-05-14 10:13:33 +0300

    add free-shape.module

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ import { ClicksModule } from './modules/clicks.module';
 import { BackgroundModule } from './modules/background.module';
 import { ShapeModule } from './modules/shape.module';
 import { ShowMessageModule } from './modules/showMessage.module';
+import { FreeShape } from './modules/free-shape.module';
 
 const menu = new ContextMenu('.menu');
 const countlicks = new ClicksModule('Считать клики (за 3 секунды)');
 const createShape = new ShapeModule('Создать фигуру');
 const changeBackground = new BackgroundModule('Поменять цвет');
 const showMessage = new ShowMessageModule('Вызвать сообщение');
+const freeShape = new FreeShape('Произвольная фигура');
 
 menu.add(countlicks);
 menu.add(createShape);
 menu.add(changeBackground);
 menu.add(showMessage);
+menu.add(freeShape);
